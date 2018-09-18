ec61655ede6899a85dc2859676e211e0ffc929bf
Andrew Qu <qua@rpi.edu>
2018-09-18 16:47:55 -0400

    Initial Commit

---
+++ ShuttleTrackeriOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShuttleTrackeriOS
+//
+//  Created by Andrew Qu on 9/18/18.
+//  Copyright © 2018 WTG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
