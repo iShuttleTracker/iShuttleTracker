fd79d0cd54ed7d3fb19bf504393a553dd574256a
Andrew Qu <qua@rpi.edu>
2018-09-27 00:48:22 -0400

    initial work on routes son

---
+++ ShuttleTrackeriOS/parsing.swift
@@ -0,0 +1,51 @@
+//
+//  parsing.swift
+//  ShuttleTrackeriOS
+//
+//  Created by Andrew Qu on 9/26/18.
+//  Copyright © 2018 WTG. All rights reserved.
+//
+
+import Foundation
+
+//json initialization
+//automatically read in Route object given the JSON file
+extension Route{
+    
+    init?(json: [String: Any]){
+        guard let id = json["id"] as? Int,
+        let color = json["color"] as? String,
+        let description = json["description"] as? String,
+        let enabled = json["enabled"] as? Bool,
+        let created = json["created"] as? String,
+        let updated = json["updated"] as? String,
+        let width = json["width"] as? Int,
+        let pointsJSON = json["points"] as? [Double:Double],
+        let name = json["name"] as? String
+        
+        
+        
+        else{
+            return nil;
+        }
+        var points: [Point]=[];
+        for string in pointsJSON{
+            guard let point = Point(latitude: Double, longitude: Double)
+                else{
+                    return nil;
+            }
+        }
+        
+        self.id = id;
+        self.name = name;
+        self.description = description;
+        self.enabled = enabled;
+        self.width=width;
+        self.stopIDs=[];
+        
+        
+    }
+    
+    
+    
+}
--- ShuttleTrackeriOS/route.swift
+++ ShuttleTrackeriOS/route.swift
@@ -17,10 +17,10 @@ struct Route{
     let enabled: Bool;
     let color: String;
     let width: Int;
-    let stopIDs: [Int]?;
-    let created: Int; //need to make time.Time for this
-    let updated: Int; //need to make time.Time for this
-    let points: [Point];
+    var stopIDs: [Int]?;
+    let created: String; //need to make time.Time for this
+    var updated: String; //need to make time.Time for this
+    var points: [Point];
 //
     init(){
         self.id=1;
@@ -30,9 +30,14 @@ struct Route{
         self.color="Black";
         self.width=1;
         self.stopIDs=[1,2,3];
-        self.created=10;
-        self.updated=10;
+        self.created="10";
+        self.updated="10";
         self.points=[Point()];
+    }
+    
+    //to add points to
+    mutating func addPoint(p: Point){
+        self.points.append(p);
     }
     
 }
