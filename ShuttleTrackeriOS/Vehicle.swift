8031bbfe60c3de9a0fe2e6a43ff419461cd79f51
Matt Czyr <mattczyr@gmail.com>
2018-10-02 17:03:49 -0400

    Initialize JSON data into Vehicle structs and return the resulting array of Vehicles in fetchVehicles()

--- ShuttleTrackeriOS/Vehicle.swift
+++ ShuttleTrackeriOS/Vehicle.swift
@@ -17,23 +17,39 @@ struct Vehicle {
     var enabled = false
     var tracker_id = ""
     
+    func printVehicle() {
+        print("ID: \(self.id)");
+        print("Name: \(self.name)");
+        print("Created: \(self.created)");
+        print("Updated: \(self.updated)");
+        print("Enabled: \(self.enabled)");
+        print("Tracker ID: \(self.tracker_id)");
+    }
 }
 
-func fetchVehicles() {
+func fetchVehicles() -> [Vehicle] {
     var vehicles:[Vehicle] = [];
     let urlString = URL(string: "https://shuttles.rpi.edu/vehicles")
     
     if let url = urlString {
         let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
             if error != nil {
-                print(error)
+                print(error!)
             } else {
                 if let usableData = data {
-                    // TODO: Use JSONSerialization to create Vehicles from the usableData and add them to the vehicles array
-                    print(String(data: usableData, encoding: .utf8)!)
+                    let json = try? JSONSerialization.jsonObject(with: usableData, options: []) as! NSArray;
+
+                    for unique in json! {
+                        print("Creating new vehicle...");
+                        let vehicle = Vehicle(json:unique as! NSDictionary);
+                        vehicle?.printVehicle();
+                        vehicles.append(vehicle!);
+                    }
                 }
             }
         }
         task.resume()
     }
+    
+    return vehicles
 }
