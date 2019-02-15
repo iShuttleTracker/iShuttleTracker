a2c46ad747d81d8d513b3d212c9200e40f9b67da
Matt Czyr <mattczyr@gmail.com>
2019-02-15 16:52:06 -0500

    Only send nearby notifications once every 60 seconds for each route ID

--- Notifications/NotificationHandler.swift
+++ Notifications/NotificationHandler.swift
@@ -12,8 +12,10 @@ import UserNotifications
 // The trips that the user has asked to be notified for
 var notifyForTrips: [Trip] = []
 
-// The route IDs of shuttles that the user should be notified for the proximity of
-var notifyForNearbyIds: [Int] = []
+// Key: The route ID of shuttles that the user should be notified of the proximity of
+// Value: A counter that is set to 60 after a notification is sent and counts down once
+//        per second. Another notification can be sent when the counter reaches 0.
+var notifyForNearbyIds: [Int:Int] = [:]
 
 /**
  Notifies the user that they should get on the shuttle now in order to reach the
@@ -49,24 +51,35 @@ func tryNotifyTime() {
  */
 func tryNotifyNearby() {
     for (id, vehicle) in vehicles {
-        if notifyForNearbyIds.contains(vehicle.last_update.route_id) {
-            // Send a notification when a shuttle is within 100 meters
-            let distance = vehicle.last_update.point.distanceFrom(p: lastLocation!)
-            if distance < 100 {
-                // Create notification content
-                let content = UNMutableNotificationContent()
-                content.title = "Nearby Shuttle"
-                content.body = "There's a shuttle \(distance) meters away."
-                content.badge = 1
-                
-                // Get notification trigger and request
-                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
-                let request = UNNotificationRequest(identifier: "ShuttleTrackerNotification", content: content, trigger: trigger)
-                
-                // Add notification to notification center
-                UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
-                print("Sending notification: \(content.body)")
+        if notifyForNearbyIds.keys.contains(vehicle.last_update.route_id) {
+            // Check if a notification has already been sent for a shuttle on this route
+            // in the last 60 seconds
+            if (notifyForNearbyIds[vehicle.last_update.route_id]! <= 0) {
+                // Send a notification when a shuttle is within 100 meters
+                let distance = vehicle.last_update.point.distanceFrom(p: lastLocation!)
+                if distance < 100 {
+                    // Create notification content
+                    let content = UNMutableNotificationContent()
+                    content.title = "Nearby Shuttle"
+                    content.body = "There's a shuttle \(distance) meters away."
+                    content.badge = 1
+                    
+                    // Get notification trigger and request
+                    let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
+                    let request = UNNotificationRequest(identifier: "ShuttleTrackerNotification", content: content, trigger: trigger)
+                    
+                    // Add notification to notification center
+                    UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+                    print("Sending notification: \(content.body)")
+                }
             }
+        }
+    }
+    
+    // Count down on each nearby notification timer that is still > 0
+    for route_id in 0..<notifyForNearbyIds.count {
+        if notifyForNearbyIds[route_id]! > 0 {
+            notifyForNearbyIds[route_id]! += -1
         }
     }
 }
