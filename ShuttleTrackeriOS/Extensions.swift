064d1c34c8e2f440cb22a6d0c80d45b3ec2c1737
Beiqi Zou <zoub@rpi.edu>
2019-02-23 14:00:36 -0500

    Convert hex string into UIColor

--- ShuttleTrackeriOS/Extensions.swift
+++ ShuttleTrackeriOS/Extensions.swift
@@ -75,6 +75,7 @@ extension ViewController: MKMapViewDelegate {
 
 }
 
+// Resize the given image
 extension UIImage {
     func resizeImage(targetSize: CGSize) -> UIImage {
         let size = self.size
@@ -89,5 +90,26 @@ extension UIImage {
         UIGraphicsEndImageContext()
         
         return newImage!
+    }
+}
+
+// Convert hex string into UIColor
+extension UIColor {
+    convenience init(hexString: String, alpha: CGFloat = 1.0) {
+        let hexString: String = hexString.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        let scanner = Scanner(string: hexString)
+        if (hexString.hasPrefix("#")) {
+            scanner.scanLocation = 1
+        }
+        var color: UInt32 = 0
+        scanner.scanHexInt32(&color)
+        let mask = 0x000000FF
+        let r = Int(color >> 16) & mask
+        let g = Int(color >> 8) & mask
+        let b = Int(color) & mask
+        let red   = CGFloat(r) / 255.0
+        let green = CGFloat(g) / 255.0
+        let blue  = CGFloat(b) / 255.0
+        self.init(red:red, green:green, blue:blue, alpha:alpha)
     }
 }
