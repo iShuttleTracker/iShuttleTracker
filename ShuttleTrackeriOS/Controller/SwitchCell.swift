1f7929f5c3879123119d1e2ed432246137616676
Beiqi Zou <zoub@rpi.edu>
2019-04-12 15:29:36 -0400

    Add custom cells and custom table view controller

---
+++ ShuttleTrackeriOS/Controller/SwitchCell.swift
@@ -0,0 +1,24 @@
+//
+//  SwitchCell.swift
+//  ShuttleTrackeriOS
+//
+//  Created by Beiqi Zou on 4/12/19.
+//  Copyright © 2019 WTG. All rights reserved.
+//
+
+import UIKit
+
+class SwitchCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
