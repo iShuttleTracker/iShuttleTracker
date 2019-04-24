0dca16877373741c9fdd56c28b676708765332c5
Matt Czyr <mattczyr@gmail.com>
2019-04-24 13:56:46 -0400

    Created writeToEndOfFile(filename, text) function in DataHandler to be used for logging shuttle estimation data; refactored some DataHandler code and relevant documentation to be more general instead of relating it only to JSON

--- Parse/DataHandler.swift
+++ Parse/DataHandler.swift
@@ -1,5 +1,5 @@
 //
-//  CacheJSON.swift
+//  DataHandler.swift
 //  ShuttleTrackeriOS
 //
 //  Created by Matt Czyr on 10/4/18.
@@ -9,11 +9,9 @@
 import Foundation
 
 /**
- Checks whether or not a file exists in the application's documents
- directory.  If the file does not exist, the data will need to be
- fetched and written.
- - Parameter filename: The name of the file to check the existance of,
-   relative to the application's documents directory
+ Checks whether or not a file exists in the application's documents directory.  If the file does not exist,
+ the data will need to be fetched and written.
+ - Parameter filename: The name of the file to check the existance of, relative to the app's documents directory
  - Returns: Whether or not the file exists
  */
 func fileExists(filename: String) -> Bool {
@@ -25,39 +23,57 @@ func fileExists(filename: String) -> Bool {
 }
 
 /**
- Writes JSON data to a given file in the application's documents directory.
+ Writes data to a file in the application's documents directory.
  - Parameters:
-   - filename: The name of the file to write to, relative to the
-     application's documents directory
+   - filename: The name of the file to write to, relative to the app's documents directory
    - data: The data to the write to the file
  */
-func writeJSON(filename: String, data: String) {
+func writeToFile(filename: String, data: String) {
     if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
         let fileURL = dir.appendingPathComponent(filename)
         do {
             try data.write(to: fileURL, atomically: false, encoding: .utf8)
         } catch let error as NSError {
-            print("Error writing JSON data to \(filename):")
+            print("Error writing data to \(filename):")
             print(error)
         }
     }
 }
 
 /**
- Reads JSON data from the file and returns it as a String.
- - Parameter filename: The name of the file to write to, relative to the
-   application's documents directory
- - Returns: A String corresponding to the contents of the file. Returns an
-   empty String if there is an error reading the file.
+ Writes text to the end of a file in the application's documents directory.
+ - Parameters:
+   - filename: The name of the file to write to, relative to the app's documents directory
+   - text: The text to the write to the end of the file
  */
-func readJSON(filename: String) -> String {
+func writeToEndOfFile(filename: String, text: String) {
+    if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+        let fileURL =  dir.appendingPathComponent(filename)
+        if fileExists(filename: filename) {
+            if let fileHandle = FileHandle(forWritingAtPath: fileURL.path) {
+                let data = text.data(using: String.Encoding.utf8)!
+                fileHandle.seekToEndOfFile()
+                fileHandle.write(data)
+                fileHandle.closeFile()
+            }
+        }
+    }
+}
+
+/**
+ Reads data from the file and returns it as a String.
+ - Parameter filename: The name of the file to write to, relative to the app's documents directory
+ - Returns: A String corresponding to the contents of the file. Returns an empty String if there is an
+            error reading the file.
+ */
+func readFromFile(filename: String) -> String {
     if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
         let fileURL = dir.appendingPathComponent(filename)
         do {
             let routeText = try String(contentsOf: fileURL, encoding: .utf8)
             return routeText
         } catch let error as NSError {
-            print("Error reading JSON data from \(filename):")
+            print("Error reading data from \(filename):")
             print(error)
         }
     }
