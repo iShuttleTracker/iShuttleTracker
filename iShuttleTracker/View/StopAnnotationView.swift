f05adce83c0cd6e6b123821e9ab5097bc79a782c
Matt Czyr <mattczyr@gmail.com>
2019-08-09 16:32:06 -0400

    Remove unnecessary code from StopAnnotationView

--- iShuttleTracker/View/StopAnnotationView.swift
+++ iShuttleTracker/View/StopAnnotationView.swift
@@ -12,22 +12,8 @@ class StopAnnotationView: MKAnnotationView {
     
     override var annotation: MKAnnotation? {
         willSet {
-            guard let stop = newValue as? StopAnnotation else { return }
+            guard let _ = newValue as? StopAnnotation else { return }
             canShowCallout = true
-            /*calloutOffset = CGPoint(x: -5, y: 5)
-             rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
-             
-             // Initialize, rotate, resize, and color the image
-             var tempImage = UIImage(named: shuttle.imageName!)!
-             tempImage = tempImage.rotate(radians: Float(Float(shuttle.heading - 45) * Float(Float.pi / 180)))!
-             tempImage = tempImage.imageWithSize(size: CGSize(width: tempImage.size.width * 1.5, height: tempImage.size.height * 1.5))
-             var color = "#cfcfcf"
-             if (routes[shuttle.route_id] != nil) {
-             color = routes[shuttle.route_id]!.color
-             }
-             tempImage = tempImage.imageWithColor(color1: UIColor(hexString: color))
-             
-             image = tempImage*/
         }
     }
     
